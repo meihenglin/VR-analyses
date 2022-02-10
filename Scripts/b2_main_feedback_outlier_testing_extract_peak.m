46d310ff7b9217a9715216343dfe6494544c6fb8
Mei-Heng Lin <51332992+meihenglin@users.noreply.github.com>
2022-02-10 15:57:50 -0500

    Add files via upload

--- Scripts/b2_main_feedback_outlier_testing_extract_peak.m
+++ Scripts/b2_main_feedback_outlier_testing_extract_peak.m
@@ -11,28 +11,24 @@ data_points2 = 2200:3300;
 freq1 = 4:8;
 
 %change chan based on channel of interest
-chan = 'P8';
+chan = 'Fz';
 
 subject = ':';
 
 %% load file
 
-load([folders.FB_TFoutput, '\A1_POWevoked_', chan, '.mat'],'ePOW_All_FB_subj');
+load([path, '\A1_POWevoked_', chan, '.mat'],'ePOW_All_FB_subj');
 
 %% conduct outlier testing by power
-% each power contains four subplots by condition
 
 figure;
         
-pw_name = 'ePOW';
-tar_v_name = strcat([pw_name,'_All_FB_subj']);
-tarvar = eval(tar_v_name);
+tarvar = eval(strcat('ePOW_All_FB_subj'));
 master_table = [];
 
 for q = 1:size(tarvar,1)
     data_sub = squeeze(mean(tarvar(q,freq1,:),2));
     master_table(q,:) = data_sub(data_points2,1);
-    subplot(1,1,1)
     scatter(time_samples2,master_table(q,:),1);
     hold on
     disp(q)
@@ -50,7 +46,7 @@ plot(time_samples2,threshd2,'k:');
 plot(time_samples2,mean_ROI,'k:');
 legend;
 
-title([tar_v_name, '_', chan], 'Interpreter','none');
+title(['ePOW_All_FB_subj_', chan], 'Interpreter','none');
 
 master_table(29,:) = mean_ROI;
 master_table(30,:) = threshd1;
@@ -59,10 +55,10 @@ master_table(31,:) = threshd2;
 o=0;
 outlie_sub = [];
 
-for sub = 1:size(master_table,1)-3 % subject 
+for sub = 1:(size(master_table,1)-3) % subject 
     for i = 1:size(master_table,2) % time
         dp_pow = master_table(sub,i);
-        thres = master_table(30,i);
+        thres = master_table(30,i); %uf exceeding threshold (only upper bound is set here)
         if dp_pow > thres
             o = o + 1;
             outlie_sub(o,1) = sub;
@@ -83,33 +79,33 @@ master_table = cat(1, master_variable_list, master_table);
 warning( 'off', 'MATLAB:xlswrite:AddSheet' ) ;           
 
 
-xlsxfilename = [folders.FB_extract_params,'\OT_',pw_name,'_',chan,'_theta_4_8.xlsx']; % defined the excel filename
+xlsxfilename = [folders.FB_extract_params,'\OT_ePOW_',chan,'_theta_4_8.xlsx']; % defined the excel filename
 table = array2table(master_table); % covnert to table
-writetable(table,xlsxfilename, 'Sheet', [tar_v_name], 'Range',(['C1:APK35'])); % write to excel
+writetable(table,xlsxfilename, 'Sheet', 'ePOW_All_FB_subj', 'Range',(['C1:APK35'])); % write to excel
 sub_num = [1:28]';
 
 asub_num = [1,2,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,29,30,31]';
 table = array2table([sub_num,asub_num], 'VariableNames',{'subject_num_in_matfiles','subject_ID_num'}); % conver them into the table
-writetable(table,xlsxfilename, 'Sheet', [tar_v_name], 'Range',(['A2:B36']))
+writetable(table,xlsxfilename, 'Sheet', 'ePOW_All_FB_subj', 'Range',(['A2:B36']))
 
 [a, b] = max(mean_ROI);
 peak_lat = b-301;
 
-xlswrite(xlsxfilename,peak_lat, [tar_v_name],'A36')
+xlswrite(xlsxfilename,peak_lat, 'ePOW_All_FB_subj','A36')
 
 if isempty(outlie_sub) ~= 1
-    xlswrite(xlsxfilename,outlie_sub, [tar_v_name],'A40')
+    xlswrite(xlsxfilename,outlie_sub, 'ePOW_All_FB_subj','A40')
 end
 
 
-xlswrite(xlsxfilename,{'peak_lat (post-stim ms)'}, [tar_v_name],'A35')
-xlswrite(xlsxfilename,{'average'}, [tar_v_name],'B31')
-xlswrite(xlsxfilename,{'average+3SD'}, [tar_v_name],'B32')
-xlswrite(xlsxfilename,{'average-3SD'}, [tar_v_name],'B33')
-xlswrite(xlsxfilename,{'sub_matID'}, [tar_v_name],'A39')
-xlswrite(xlsxfilename,{'time_pts_outlier'}, [tar_v_name],'B39')
+xlswrite(xlsxfilename,{'peak_lat (post-stim ms)'}, 'ePOW_All_FB_subj','A35')
+xlswrite(xlsxfilename,{'average'}, 'ePOW_All_FB_subj','B31')
+xlswrite(xlsxfilename,{'average+3SD'}, 'ePOW_All_FB_subj','B32')
+xlswrite(xlsxfilename,{'average-3SD'}, 'ePOW_All_FB_subj','B33')
+xlswrite(xlsxfilename,{'sub_matID'}, 'ePOW_All_FB_subj','A39')
+xlswrite(xlsxfilename,{'time_pts_outlier'}, 'ePOW_All_FB_subj','B39')
 
-savefig([[folders.FB_figures],'\OT_',pw_name,'_',chan,'_theta_4_8.fig'])
+savefig([[folders.FB_figures],'\OT_ePOW_',chan,'_theta_4_8.fig'])
     
 
 
